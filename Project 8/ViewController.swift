d59ceccaf12bc13d5645ac58112bfc0ea2565b68
keertirajmalik <keertirajmalik@gmail.com>
2022-01-25 22:14:21 +0530

    adding buttons in the buttons view

--- Project 8/ViewController.swift
+++ Project 8/ViewController.swift
@@ -96,9 +96,24 @@ class ViewController: UIViewController {
             
         ])
         
-        cluesLabel.backgroundColor = .red
-        answersLabel.backgroundColor = .blue
-        buttonsView.backgroundColor = .green
+        let width  = 150
+        let height = 80
+        
+        for row in 0..<4 {
+            for col in 0..<5 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                
+                letterButton.setTitle("WWW", for: .normal)
+                
+                let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView.addSubview(letterButton)
+                
+                letterButtons.append(letterButton)
+            }
+        }
     }
 
     override func viewDidLoad() {
